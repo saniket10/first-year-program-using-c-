d05180b55d9eb15557cb25d065d4084b51aff665
Saniket-hub <saniketubhare2006@gmail.com>
2026-05-14 15:47:54 +0530

    Area of square
    “Program to calculate the area of a square in C.”

---
+++ Area of square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  // finding area of square 
+  int side ,area ;
+  printf("enter the side:\t");
+  scanf("%d",&side );
+  area = side*side ;
+  printf("the area sq is %d",area  );
+  
+  return 0;
+}
