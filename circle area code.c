1bfd8278a061d73e4caee058784261113ac7925a
Saniket-hub <saniketubhare2006@gmail.com>
2026-05-14 15:44:13 +0530

    circle code to find the circle area
    A simple C program to calculate the area of a circle using the radius provided by the user.”

---
+++ circle area code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  // finding circle area 
+  float radius,area ;
+  printf("enter the circle radius:\t");
+  scanf("%f",&radius);
+  area = 3.14*radius*radius;
+  printf("the area of circle is %f",area );
+  
+  return 0;
+}
